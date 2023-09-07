38b07a41a21d224019db5b7013df8a826fb91be5
Phạm Đình Võ <132974276+KallySchwarzt@users.noreply.github.com>
2023-09-07 16:45:01 +0700

    add-chart

--- MobileShop.Web/Areas/Admin/Controllers/HomeAdminController.cs
+++ MobileShop.Web/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MobileShop.Web.Models;
+using Newtonsoft.Json;
 
 namespace MobileShop.Web.Areas.Admin.Controllers
 {
-    public class HomeAdminController : Controller
-    {
-        public IActionResult Index()
-        {
+	[Area("Admin")]
+	[Route("HomeAdmin")]
+	public class HomeAdminController : Controller
+	{
+		[Route("Index")]
+		public IActionResult Index()
+		{
+			List<DataPoint> dataPoints1 = new List<DataPoint>{
+				new DataPoint("12/2023", 22),
+				new DataPoint("12/2023", 36),
+				new DataPoint("12/2023", 42),
+				new DataPoint("12/2023", 51),
+				new DataPoint("12/2023", 46),
+			};
+            List<DataPoint> dataPoints2 = new List<DataPoint>{
+                new DataPoint("12/2023", 22),
+                new DataPoint("12/2023", 36),
+                new DataPoint("12/2023", 42),
+                new DataPoint("12/2023", 51),
+                new DataPoint("12/2023", 46),
+            };
+            List<DataPoint> dataPoints3 = new List<DataPoint>{
+                new DataPoint("12/2023", 22),
+                new DataPoint("12/2023", 36),
+                new DataPoint("12/2023", 42),
+                new DataPoint("12/2023", 51),
+                new DataPoint("12/2023", 46),
+            };
+            List<DataPoint> dataPoints4 = new List<DataPoint>{
+                new DataPoint("12/2023", 22),
+                new DataPoint("12/2023", 36),
+                new DataPoint("12/2023", 42),
+                new DataPoint("12/2023", 51),
+                new DataPoint("12/2023", 46),
+            };
+
+            ViewBag.DataPoints1 = JsonConvert.SerializeObject(dataPoints1);
+            ViewBag.DataPoints2 = JsonConvert.SerializeObject(dataPoints2);
+            ViewBag.DataPoints3 = JsonConvert.SerializeObject(dataPoints3);
+            ViewBag.DataPoints4 = JsonConvert.SerializeObject(dataPoints4);
+
             return View();
-        }
-    }
+		}
+	}
 }
---
+++ MobileShop.Web/Models/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace MobileShop.Web.Models
+{
+	[DataContract]
+	public class DataPoint
+	{
+		public DataPoint(string label, double y)
+		{
+			this.label = label;
+			this.Y = y;
+		}
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "label")]
+		public string label;
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "y")]
+		public Nullable<double> Y = null;
+	}
+}
